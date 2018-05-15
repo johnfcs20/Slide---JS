91c7d2e4eaa373cbe4b03a1b2894afb09fc36891
johnfcs20 <john20_f@hotmail.com>
2018-05-15 11:58:55 -0400

    Carruser de imagenes con JS

---
+++ js/slide.js
@@ -0,0 +1,126 @@
+/*==============================
+  = Propiedades                =
+  ==============================*/
+
+var p = {
+
+    paginacion: document.querySelectorAll("#paginacion li"),
+    item: 0,
+    cajaSlide: document.querySelector("#slide ul"),
+    anicamcionSlide: "fade",
+    imgSlide: document.querySelectorAll("#slide ul li"),
+    avanzar: document.querySelector("#slide #avanzar"),
+    retroceder: document.querySelector("#slide #retroceder"),
+    velocidadSlide: 3000,
+    formatearloop: false,
+}
+
+
+/*==============================
+= metodos                      =
+================================*/
+
+var m = {
+
+    inicioSlide: function() {
+
+        for (var i = 0; i < p.paginacion.length; i++) {
+
+            p.paginacion[i].addEventListener("click", m.paginacionSlide)
+            p.imgSlide[i].style.width = (100 / p.paginacion.length) + "%"
+        }
+
+        p.avanzar.addEventListener("click", m.avanzar)
+        p.retroceder.addEventListener("click", m.retroceder)
+        m.intervalo();
+        p.cajaSlide.style.width = (p.paginacion.length * 100) + "%"
+    },
+
+    paginacionSlide: function(item) {
+
+        p.item = item.target.parentNode.getAttribute("item") - 1;
+
+        m.movimientoSlide(p.item);
+    },
+
+    avanzar: function() {
+
+        if (p.item == p.imgSlide.length - 1) {
+
+            p.item = 0;
+
+        } else {
+            p.item++;
+
+        }
+
+
+        m.movimientoSlide(p.item);
+
+    },
+
+    retroceder: function() {
+
+
+        if (p.item == 0) {
+
+            p.item = p.imgSlide.length - 1;
+
+        } else {
+            p.item--;
+
+        }
+
+    },
+
+    movimientoSlide: function(item) {
+
+        p.formatearloop = true;
+
+        p.cajaSlide.style.left = item * -100 + "%";
+        for (var i = 0; i < p.paginacion.length; i++) {
+            p.paginacion[i].style.opacity = .5;
+        }
+
+        p.paginacion[item].style.opacity = 1;
+
+
+        if (p.anicamcionSlide == "slide") {
+
+            p.cajaSlide.style.transition = ".7s left ease-in-out";
+
+        }
+
+        if (p.anicamcionSlide == "fade") {
+
+            p.imgSlide[item].style.opacity = 0;
+
+            setTimeout(function() {
+                p.imgSlide[item].style.transition = ".7s opacity ease-in-out";
+                p.imgSlide[item].style.opacity = 1;
+            }, 500)
+
+        }
+
+    },
+
+
+    intervalo: function() {
+
+
+        setInterval(function() {
+            if (p.formatearloop) {
+                p.formatearloop = false
+            } else {
+                m.avanzar();
+            }
+
+        }, p.velocidadSlide)
+
+    }
+
+
+}
+
+
+m.inicioSlide();
